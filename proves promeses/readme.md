2886fc271e5d26fb0e8f18d5fbd7cdac23e616e6
xxjcaxx <xxjcaxx@yahoo.es>
2020-09-21 17:41:12 +0200

    promeses

---
+++ proves promeses/readme.md
@@ -0,0 +1,4 @@
+Script basat en https://github.com/googlesamples/web-fundamentals/tree/gh-pages/fundamentals/primers
+de l'artícul https://developers.google.com/web/fundamentals/primers/promises?hl=es
+
+I adaptat i simplificat per a la classe de promeses de DWEC 2020
--- proves promeses/scripts.js
+++ proves promeses/scripts.js
@@ -1,3 +1,10 @@
+/*
+Script basat en https://github.com/googlesamples/web-fundamentals/tree/gh-pages/fundamentals/primers
+de l'artícul https://developers.google.com/web/fundamentals/primers/promises?hl=es
+
+I adaptat i simplificat per a la classe de promeses de DWEC 2020
+*/
+
 let fakeSlowNetwork = 1;
 let storyDiv;
 
