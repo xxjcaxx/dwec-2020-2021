af64b33f2d59df4184533c339484947f14103b70
xxjcaxx <xxjcaxx@yahoo.es>
2020-09-24 12:27:22 +0200

    ajax jquery

--- jquery/scripts.js
+++ jquery/scripts.js
@@ -1,5 +1,29 @@
 $(function () {
-    $('#header h1').on('click',function(){
-        console.log('click en',$(this));
+  "use strict";
+  $("#header").on("click", cargarDatosSimple);
+
+  function cargarDatos() {
+    $.ajax({ type: "get", url: "datos.json", data: "data", dataType: "json",
+      success: function (response) {
+        console.log("exito", response);
+      },
+      error: function (xhr, status) {
+        console.error("error", xhr, status);
+      },
+      complete: function (xhr, status) {
+        console.log("completo", xhr, status);
+      },
     });
+  }
+  function cargarDatosSimple(){
+      $.get('datos.json',{'nombre':'John','apellido':'doe'},function(respuesta){
+        console.log(respuesta);
+      }).done(function(){
+          console.log('exito');
+      }).fail(function(){
+          console.error('error');
+      }).always(function(){
+          console.log('siempre');
+      });
+  }
 });
