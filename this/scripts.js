02673c6ad680071d03ef66f4e7d82bd1fc015e7b
xxjcaxx <xxjcaxx@yahoo.es>
2020-10-02 19:58:39 +0200

    this that

--- this/scripts.js
+++ this/scripts.js
@@ -1,15 +1,26 @@
 (function () {
-  "use strict";
+  "use strict";   // Prova a descomentar
   document.addEventListener("DOMContentLoaded", function () {
     var colours = ["red", "green", "blue"];
     document.getElementById("header").addEventListener("click", function () {
       // this és una referència al clicat
       var that = this;
-      colours.forEach(function (element, index) {
-        console.log(this, that, index, element);
+      colours.forEach(function (element, index, array) {
+        console.log(this, that, index, element, array);
         // this és undefined
         // that és el que s'ha clicat
       });
+      colours.forEach( (element, index, array) =>  
+                        console.log(this, that, index, element, array)
+        // Les funcions fletxa no tenen context d'execució
+        // this és el que s'ha clicat
+        // that és el que s'ha clicat
+      );
+      colours.forEach(function (element, index, array) {
+        console.log(this, that, index, element, array);
+        // this és undefined
+        // that és el que s'ha clicat
+      }, this);
     });
   });
 })();
