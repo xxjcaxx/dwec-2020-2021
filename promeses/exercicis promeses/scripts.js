4dd4598ccdef832e1f0a5091e65b171583ec5ae1
xxjcaxx <jocasal@gmail.com>
2020-11-17 19:35:52 +0100

    exercici4 promeses

--- promeses/exercicis promeses/scripts.js
+++ promeses/exercicis promeses/scripts.js
@@ -1,6 +1,7 @@
 (function () {
     "use strict";
     document.addEventListener("DOMContentLoaded", function () {
+
         let p1 = document.querySelector('#p1');
 
         let promesa1 = new Promise(function executar(resolver, rechazar) {
@@ -10,8 +11,7 @@
         });
         promesa1.then(function () {
             p1.innerHTML = '<p>Promesa 1</p>';
-        }
-        )
+        });
 
         /// Fes que al fer click al p2, escriga 3 paraules en 2 segons de diferència
         let p2 = document.querySelector('#p2');
@@ -28,11 +28,13 @@
                 , 2000 * Math.random());
         });
 
+        function exercici2(){
         promesa2()
             .then(() => promesa2segundos('primera'))
             .then(() => promesa2segundos('segunda'))
             .then(() => promesa2segundos('tercera'));
-
+        }
+        exercici2();
 
         //// Transforma, per a p3, el que fa p2 però en Promise.all
 
@@ -51,13 +53,13 @@
             }
                 , 2000 * Math.random());
         });
-
-        /*   promesa3().then(()=> Promise.all([  // Paraleles
+/*
+           promesa3().then(()=> Promise.all([  // Paraleles
               promesa3segundos('primera'),
               promesa3segundos('segunda'),
               promesa3segundos('tercera')
-              ])); */
-
+              ])); 
+*/
         ////////////// Ara fes que siguen seqüencials
 
         /// Solució en reduce 
@@ -76,7 +78,57 @@
             /// En p4 fes que, al fer click, retorne una promesa 
             // de restaurar els gifs de les promeses anteriors i la seua funcionalitat
             // Tardarà 1 segon entre cada restauració
+            let p4 = document.querySelector('#p4');
+            //let promesa4 = () => new Promise((resolver) => {
+            //    p4.addEventListener('click', () => {
+            //        restaurarP1();
+            //    });
+            //});
 
+            function restaurarP1(){
+                return new Promise((r)=>{ setTimeout(()=>{
+                    let p1 = document.querySelector('#p1');
+                    p1.innerHTML = 'P1 <img src="loading.gif" alt="">';
+                    let pro1 = new Promise(function executar(resolver, rechazar) {
+                        p1.addEventListener('click', function click(event) {
+                            resolver();
+                        });
+                    }).then(function () {
+                        p1.innerHTML = '<p>Promesa 1</p>';
+                    });
+                    r();
+                },200)});
+            }
+
+            function restaurarP2(){
+                return new Promise((r)=>{ setTimeout(()=>{
+                p2.innerHTML = 'P2 <img src="loading.gif" alt="">';
+                
+                exercici2();
+                r();
+            },200)});
+            }
+
+            function restaurarP3(){
+                return new Promise((r)=>{ setTimeout(()=>{
+                p3.innerHTML = 'P3 <img src="loading.gif" alt="">';
+                
+                promesa3()
+                .then(() =>
+                ['primera', 'segunda', 'tercera'].reduce( 
+                    (previousPromise, current) => { // current són els elements de l'array
+                        return previousPromise.then(
+                            () => promesa3segundos(current))
+                    }, Promise.resolve() // segon argument de reduce és el primer element (PreviousPromise)
+                ));              // El primer element és una promesa buida per començar
+                r();
+            },200)});
+            }
+
+            //promesa4().then(restaurarP1);
+            p4.addEventListener('click', () => {
+                restaurarP1().then(restaurarP2).then(restaurarP3);
+            });
 
             /////// En p5 fes que, al fer click, genere un array de numeros aleatoris,
             // després cridarà a una promesa per número que tardarà el mateix que eixe número
