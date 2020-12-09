7786ea8836acf0addf27d33eb6fd702e028a34d0
xxjcaxx <xxjcaxx@yahoo.es>
2020-12-09 11:38:52 +0100

    repas promeses

---
+++ promeses/repas promeses i fetch/repas.js
@@ -0,0 +1,25 @@
+class Partido{
+    constructor(datosPartido){
+        Object.assign(this,datosPartido);
+    }
+    dibujar() {
+        // TODO
+    }
+}
+
+
+( () => {
+    "use strict";
+
+    function mostrarPartidos(){
+        // TODO descarregar la llista de partits
+        // TODO fer un array d'objectes partit
+        // TODO cridar a la funció dibujar de cada partit
+        // TODO Crear una taula amb totes les dades
+    }
+
+    document.addEventListener("DOMContentLoaded", function () {
+        let button = document.querySelector('mostrar-partidos',mostrarPartidos);
+
+    });
+})();
